a32a57c3b994c697be2d6f6701e3130fdfb79a6f
nadaadzani <148736809+nadaadzani@users.noreply.github.com>
2023-10-29 21:18:08 +0700

    Add files via upload
    updated list

--- kyu8Exercise.js
+++ kyu8Exercise.js
@@ -79,3 +79,95 @@ function makeNegative2(num) {   // Using a ternary operator to cleverly evaluate
 //         return num;
 //     }
 // }
+
+
+// Total amounts of points
+
+// Our football team has finished the championship.
+
+// Our team's match results are recorded in a collection of strings. Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+// Notes:
+
+// our team always plays 10 matches in the championship
+// 0 <= x <= 4
+// 0 <= y <= 4
+
+// Best answer:
+const points = games => games.reduce((output,current) => {      // Utilizing reduce() method again and ternary operator for shorter write
+    return output += current[0] > current[2] ? 3 : current[0] === current[2] ? 1 : 0;
+  },0)
+
+// function points(games) {
+//   return games.reduce((output,current)=>{
+//     let x = parseInt(current[0]);
+//     let y = parseInt(current[2]);
+//     let value= x>y ? 3 : x===y ? 1 : 0;
+//     return output+value;
+//   },0)
+// }
+
+// My answer:
+function points(games) {        // Could've used for...of loop or forEach() method to access an array easier
+    let total = 0;
+    for (let i = 0; i < games.length; i++) {
+      let x = Number((games.slice(i, i+1).toString()[0]));
+      let y = Number((games.slice(i, i+1).toString()[2]));
+      if (x > y) {
+      total += 3
+      } else if (x === y) {
+      total += 1
+      } else if (x < y) {
+      total += 0
+      }
+    }
+    return total;
+  }
+
+
+// Grade book
+// Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+  
+// Numerical Score	Letter Grade
+// 90 <= score <= 100	'A'
+// 80 <= score < 90	'B'
+// 70 <= score < 80	'C'
+// 60 <= score < 70	'D'
+// 0 <= score < 60	'F'
+// Tested values are all between 0 and 100. Theres is no need to check for negative values or values greater than 100.
+
+// Best answer: 
+function getGrade (s1, s2, s3) {
+    avg = (s1+s2+s3)/3;
+    if (avg < 60)  return "F";
+      else if (avg < 70) return "D";
+      else if (avg < 80) return "C";
+      else if (avg < 90) return "B";
+      else return "A";
+}
+
+// My answer:
+function getGrade(s1, s2, s3) {     // More explicit ig
+    let total = s1 + s2 + s3;
+    let average = total / 3;
+    if (average >= 0 && average < 60) {
+      return 'F';
+    } else if (average >= 60 && average < 70) {
+      return 'D';
+    } else if (average >= 70 && average < 80) {
+      return 'C';
+    } else if (average >= 80 && average < 90) {
+      return 'B';
+    } else if (average >= 90 && average <= 100) {
+      return 'A';
+    }
+  }
